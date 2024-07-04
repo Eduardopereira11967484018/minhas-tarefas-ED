57dbb80866d351641675e81becbfbb80f678c8de
Eduardopereira11967484018 <eduardoperira.lima@yahoo.com.br>
2024-07-04 12:04:02 -0300

    Adicionar efeito de hover ao componente Card

---
+++ src/styled/conteinar/ListaDeTarefas/index.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+const ListasDeTarefas = () => (
+  <main>
+    <p> 2 tarefas marcardas com: &quot;categoria&Ildquo;termo&Idquo;</p>
+  </main>
+)
+export default ListasDeTarefas
---
+++ src/styled/conteinar/Sidebar/index.tsx
@@ -0,0 +1,24 @@
+import FilterCard from '../../../components/FilterCard'
+
+import * as S from './styles'
+
+/* eslint-disable prettier/prettier */
+const Sidebar = () =>(
+<S.Aside>
+  <div>
+  <input type="text" placeholder="Buscar" />
+  <S.Filter>
+    <FilterCard ativo={true} contador={1} legenda={'Pendente'} />
+    <FilterCard ativo={true} contador={2} legenda={'Concluidas'} />
+    <FilterCard ativo={true} contador={3} legenda={'Urgentes'} />
+    <FilterCard ativo={true} contador={4} legenda={'Importantes'} />
+    <FilterCard ativo={true} contador={5} legenda={'Normal'} />
+    <FilterCard ativo={true} contador={6} legenda={'Todas'} />
+
+  </S.Filter>
+  </div>
+
+ </S.Aside>
+)
+
+export default Sidebar
