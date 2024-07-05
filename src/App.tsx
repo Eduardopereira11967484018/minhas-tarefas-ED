491bb39c68bb188e781eacfb8679c497295ec873
Eduardopereira11967484018 <eduardoperira.lima@yahoo.com.br>
2024-07-05 10:16:18 -0300

    conclusao do projetoi

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
-/* eslint-disable prettier/prettier */
+import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import EstiloGlobal, { Container } from "./styled"
-import ListaDeTarefas from "./components/conteinar/ListaDeTarefas/index"
-import Sidebar from "./components/conteinar/Sidebar/index"
+import EstiloGlobal, { Container } from "./styles";
 
+import store from "./store";
+import Home from "./pages/Home";
+import Cadastro from "./pages/Cadastro";
 
+const rotas = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/novo",
+    element: <Cadastro />,
+  },
+]);
 
 function App() {
   return (
-  <>
-  <EstiloGlobal />
-  <Container>
-    <Sidebar/>
-    <ListaDeTarefas />
-  </Container>
-  </>
-  )
+    <Provider store={store}>
+      <EstiloGlobal />
+      <Container>
+        <RouterProvider router={rotas} />
+      </Container>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
